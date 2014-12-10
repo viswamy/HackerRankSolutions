5ff55a65c5df8b6899bf7f0d47e1fc083a77bd70
Virupaksha Swamy U M <vswamy@outlook.com>
2014-12-10 17:51:00 +0530

    Love letter mystery

--- src/com/vswamy/hackerranksolutions/Main.java
+++ src/com/vswamy/hackerranksolutions/Main.java
@@ -12,7 +12,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Problem problem = new AlternatingCharacters();
+        Problem problem = new LoveLetterMystery();
         problem.run();
         return;
     }
---
+++ src/com/vswamy/hackerranksolutions/problems/LoveLetterMystery.java
@@ -0,0 +1,38 @@
+package com.vswamy.hackerranksolutions.problems;
+
+import java.util.Scanner;
+
+import com.vswamy.hackerranksolutions.interfaces.Problem;
+
+/**
+ * @author vswamy
+ * @Problem statement: https://www.hackerrank.com/challenges/maximizing-xor
+ */
+
+public class LoveLetterMystery implements Problem
+{
+
+    @Override
+    public void run()
+    {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfTestCases = scanner.nextInt();
+        scanner.nextLine();
+        for(int i = 0; i < numberOfTestCases; i++)
+        {
+            String str = scanner.nextLine();
+            printMinimumCount(str);
+        }
+    }
+
+    private void printMinimumCount(String str)
+    {
+        int strLength = str.length();
+        int count = 0;
+        for(int i = 0; i < strLength/2; i++)
+        {
+            count += Math.abs(str.charAt(i) - str.charAt(strLength-1-i));
+        }
+        System.out.println(count);
+    }
+}
